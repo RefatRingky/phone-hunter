d8ad82c47827f01218b5e71e6fe89f6cfafa8cbd
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-03-01 16:14:49 +0600

    others section done commit

--- js/api.js
+++ js/api.js
@@ -83,7 +83,12 @@ const displayDetails = (details) => {
          <p>Phone-Name:${details.name}</p>
          <p>ReleaseDate:${details.releaseDate}</p>
          <p>Sensor:${details.mainFeatures.sensors}</p>
-         <p>Others:${details.others}</p>
+         <p>Others:${details.others.Bluetooth}</p>
+         <p>Others:${details.others.WLAN}</p>
+         <p>Others:${details.others.NFC}</p>
+         <p>Others:${details.others.GPS}</p>
+         <p>Others:${details.others.Radio}</p>
+         <p>Others:${details.others.USB}</p>
   
     `
     phoneDetails.appendChild(div);
